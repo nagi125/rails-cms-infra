7a1fe9e2c7897b9316d63393d5ba17c33086c0c3
nagi125 <nagi125.dev@gmail.com>
2021-07-28 14:55:17 +0900

    基本設定追加

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Provider設定
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "app_name" {
+  type = string
+  default = "rails-cms-api"
+}
+
+variable "zone" {
+  type = string
+  default = "nagi-dev.jp"
+}
+
+variable "domain" {
+  type = string
+  default = "api.nagi-dev.jp"
+}
